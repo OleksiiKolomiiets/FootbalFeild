0d79623b488a733bcfbdd4de391d0582c5eb4eb2
OleksiiKolomiiets <Oleksii.Kolomiiets@onseo.biz>
2018-12-04 16:58:37 +0200

    Revert "feat: add football field."
    This reverts commit 1df1c82b0de2e3c4eef3e2782cab801b1a8ee6ca.

---
+++ FootbalFeild/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FootbalFeild
+//
+//  Created by Oleksii  Kolomiiets on 11/30/18.
+//  Copyright © 2018 Oleksii  Kolomiets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

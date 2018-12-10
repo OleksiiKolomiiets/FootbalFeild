332617e0ab8166d044a0127d341a5f122a9970db
OleksiiKolomiiets <Oleksii.Kolomiiets@onseo.biz>
2018-12-10 12:07:29 +0200

    refactor: replace enumeration for arc types.

--- FootbalFeild/Utilites/PathHelper.swift
+++ FootbalFeild/Utilites/PathHelper.swift
@@ -31,6 +31,52 @@ struct SingleLine: Line {
     let end  : CGPoint
 }
 
+enum ArcType {
+    case topLeftCorner
+    case topRightCorner
+    case bottomRightCorner
+    case bottomLeftCorner
+    case topPenalty
+    case bottomPenalty
+    case circle
+    
+    var arcStartXCorrecion: CGFloat {
+        return cos(startAngle)
+    }
+    
+    var arcStartYCorrection: CGFloat {
+        return sin(startAngle)
+    }
+    
+    var endAngle: CGFloat {
+        switch self {
+        case .circle:
+            return 2 * .pi
+        default:
+            return startAngle + 0.5 * .pi
+        }
+    }
+    
+    var startAngle: CGFloat {
+        switch self {
+        case .topLeftCorner:
+            return 0
+        case .topRightCorner:
+            return 0.5 * .pi
+        case .bottomRightCorner:
+            return .pi
+        case .bottomLeftCorner:
+            return 1.5 * .pi
+        case .topPenalty:
+            return 0.25 * .pi
+        case .bottomPenalty:
+            return 1.25 * .pi
+        case .circle:
+            return 0
+        }
+    }
+}
+
 class PathHelper: PathHelperProtocol {
     
     public var path: UIBezierPath?
